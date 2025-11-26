21ea5974a5abc43495302ee0ac061726252f977e
Vicho <vicentecastroibarra@gmail.com>
2025-11-26 19:57:21 -0300

    Generadorexcusas

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 function generarExcusas() {
-  // Seleccionar un elemento aleatorio de cada array
-  let sujetoAleatorio = who[Math.floor(Math.random() * who.length)];
-  let accionAleatoria = action[Math.floor(Math.random() * action.length)];
-  let objetoAleatorio = what[Math.floor(Math.random() * what.length)];
-  let momentoAleatorio = when[Math.floor(Math.random() * when.length)];
-
-  // Unir las partes en una frase
-  return `${sujetoAleatorio} ${accionAleatoria} ${objetoAleatorio} ${momentoAleatorio}.`;
-}
+  
+    const sujeto = who[Math.floor(Math.random() * who.length)];
+    const verbo = action[Math.floor(Math.random() * action.length)];
+    const objeto = what[Math.floor(Math.random() * what.length)];
+    const cuando = when[Math.floor(Math.random() * when.length)];
+  
+    return `${sujeto} ${verbo} ${objeto} ${cuando}.`;
+  }
 console.log(generarExcusas());
